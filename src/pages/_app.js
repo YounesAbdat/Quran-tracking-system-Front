c70c8feb3c2be7cdd119dc3a4e1a14d7d8a579be
yasmiinie <ly_zaidi@esi.dz>
2025-06-08 18:43:43 +0100

    some fixes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
 import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+            <Head>
+              <link rel="icon" type="image/png" href="/favicon.png" />
+            </Head> 
+          <Component {...pageProps} />
+    </div>);
 }
